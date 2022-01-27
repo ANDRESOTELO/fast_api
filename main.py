b2b1601cb5123d881f0ac4a7cf2c2984d986adb8
ANDRESOTELO <andresotelo2020@gmail.com>
2022-01-26 22:30:59 -0500

    ADDED | Request body

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 
 app = FastAPI()
+
+
+# Models
+
+class Person(BaseModel):
+    """Person model"""
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 
 @app.get('/api')
@@ -9,4 +28,12 @@ def home():
     """First endpoint"""
     return {'Status': 'OK',
             'message': 'API home',
-            'status': 200}
+            'status_code': 200}
+
+
+# Request and Response body
+
+@app.post('/person/new')
+def create_person(person: Person = Body(...)):
+    """Allows to create new person"""
+    return person
